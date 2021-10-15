f7b0541325cba40602d2cf67b028653e266ff7c8
Rashmi Gamage <wgrashmi98@gmail.com>
2021-10-15 20:14:19 +0530

    Code addition to app.js, ContactCard.js and ContactList.js files upto deleting contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { uuid } from 'uuidv4';
+
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import { useState, useEffect } from 'react';
 
+
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
+
+
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts,{ id : uuid(), ...contact} ]);
   };
+
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter( (contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  }
+
   useEffect( () => {
     const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (retriveContacts) setContacts(retriveContacts);
@@ -27,7 +42,7 @@ function App() {
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -3,14 +3,18 @@ import user from "../images/user.png";
 
 
 const ContactCard = (props) => {
-    //const { id, email, name}  = props.contact;
+    const { id, email, name}  = props.contact;
     return (
         <div className="item">
           <img className="ui avatar image" src={user} alt="user"/>
           <div className="content">
             <div className="header">{props.contact.name}</div>
             <div>{props.contact.email}</div>
-            <i className="trash alternate outline icon" style={{ color: "red", marginTop: "7px"} }></i>
+            <i 
+            className="trash alternate outline icon" 
+                style={{ color: "red", marginTop: "7px"} }
+            onClick={ () => props.clickHander(id)}
+            ></i>
           </div>
         </div> 
 
